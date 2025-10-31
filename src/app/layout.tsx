818df7d1787ef7f921a4731595a406341b2921c3
KishanthDev <kishanthkisna@gmail.com>
2025-10-31 17:13:24 +0530

    improved color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,7 +60,7 @@ export default async function RootLayout({
         <NuqsAdapter>
           <ThemeProvider
             attribute='class'
-            defaultTheme='system'
+            defaultTheme='blue'
             enableSystem
             disableTransitionOnChange
             enableColorScheme
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -24,7 +24,6 @@ export default function Header() {
         </div>
         <UserNav />
         <ModeToggle />
-        <ThemeSelector />
       </div>
     </header>
   );
